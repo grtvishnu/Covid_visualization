e3cb1feff40ffd5a05a483d479a511e18e3f803e
grtvishnu <optra7@gmail.com>
2020-07-08 23:12:00 +0530

    aa

--- covid.R
+++ covid.R
@@ -3,23 +3,23 @@ library(scales)
 library(lubridate)
 library(gganimate)
 library(gifski)
-cov_latest_all <- read_csv("time-series-19-covid-combined.csv")
+cov_nes <- read_csv("time-series-19-covid-combined.csv")
 
 
-cov_nes <- cov_latest_all %>% 
+cov_nes <- cov_nes %>% 
   select(Date,country=`Country/Region`, Confirmed, Recovered,Deaths)
 rm(cov_latest_all)
 
 
 cov_stat<- cov_nes %>% 
   group_by(country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   mutate(Active_case = Confirmed- Recovered)
 
 
 cov_nes %>% 
   group_by(country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   arrange(desc(Death)) %>% 
   top_n(10) %>% 
   ggplot(aes(x=reorder(country,-Death), y= Death, fill=country))+
@@ -29,7 +29,7 @@ cov_nes %>%
 
 cov_nes %>% 
   group_by(country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   arrange(desc(Confirmed)) %>% 
   top_n(10) %>% 
   ggplot(aes(x=reorder(country,-Confirmed), y= Confirmed, fill=country))+
@@ -38,7 +38,7 @@ cov_nes %>%
 
 cov_nes %>% 
   group_by(country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   arrange(desc(Recovered)) %>% 
   top_n(10) %>% 
   ggplot(aes(x=reorder(country,-Recovered), y= Recovered, fill=country))+
@@ -47,7 +47,7 @@ cov_nes %>%
 
 cov_nes %>% 
   group_by(country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   mutate(Active_case = Confirmed - Recovered) %>% 
   top_n(10) %>% 
   ggplot(aes(x=reorder(country,Active_case), y= Active_case, fill=country))+
@@ -63,7 +63,7 @@ mutate(year = lubridate::year(Date),
 
 ani1<- cov_nes %>% 
   group_by(Date,country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   arrange(desc(Death)) %>%
   filter(country=="India"|
          country=="US"|
@@ -74,13 +74,13 @@ ani1<- cov_nes %>%
   geom_line()+
   scale_y_continuous(labels = comma)+
   geom_point(size =1.5)+
-  transition_reveal(Confirmed)
+  transition_reveal(Death)
 animate(ani1, height=600, width=800, fps = 30,duration = 10, end_pause = 60, res=100)  
 
 
 ani1<- cov_nes %>% 
   group_by(Date,country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=sum(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   arrange(desc(Death)) %>%
   filter(country=="India"|
            country=="US"|
@@ -97,7 +97,7 @@ animate(ani1, height=600, width=800, fps = 30,duration = 10, end_pause = 60, res
 
 ani1<- cov_nes %>% 
   group_by(Date,country) %>% 
-  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  summarise(Death=max(Deaths),Confirmed =max(Confirmed), Recovered=max(Recovered)) %>% 
   arrange(desc(Recovered)) %>%
   filter(country=="India"|
            country=="US"|
@@ -110,3 +110,19 @@ ani1<- cov_nes %>%
   geom_point(size =1.5)+
   transition_reveal(Confirmed)
 animate(ani1, height=600, width=800, fps = 30,duration = 10, end_pause = 60, res=100)  
+
+
+
+p<- cov_nes %>% 
+  group_by(Date,country) %>% 
+  summarise(Death=sum(Deaths),Confirmed =sum(Confirmed), Recovered=sum(Recovered)) %>% 
+  arrange(desc(Death)) %>% 
+  filter(country=="India"|
+           country=="US"|
+           country=="Brazil"|
+           country=="Russia") %>% 
+  ggplot(aes(x=reorder(country,-Death), y= Death, fill=country))+
+  geom_bar(stat = "identity")+
+  scale_y_continuous(labels = comma)+
+  transition_time(Date)
+animate(p, height=600, width=800, fps = 30,duration = 10, end_pause = 60, res=100)  
